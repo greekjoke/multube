c9cee9c290a43a6715eee0fc611e693b86b20073
ak <akbsc@mail.ru>
2023-08-16 10:44:35 +0300

    settings routines

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 window.MtApp = {
 
   settings: {
-    version: 0.1,
+    version: '0.1',
+    timestamp: '',
     curLang: 'en',
     curScheme: 'default',
     curPreset: 'default',    
@@ -18,15 +19,36 @@ window.MtApp = {
 
   init: function(opt) {
     opt = opt || {};
+    
+    if (!MtUtils.hasQueryKey('reset')) {      
+      this.settingsRead();
+    } else {
+      console.info('settings loading was ignoreed by query key');
+    }
+
     this.updateMenuPresets();
+    this.updateSwithFlags('#topmenuColors', this.settings.curScheme);
+  },
+
+  releaseMenu: function(menu) {
+    menu = menu || '#topmenu';
+    $(menu).addClass('disabled');
+    setTimeout(() => $(menu).removeClass('disabled'), 100);
   },
 
   showPrompt: function(text, def, fnOk, fnCancel) {
     fnCancel = fnCancel || function() {};
-    const str = window.prompt(text, def || '');
+    const str = window.prompt(text, def || '');    
     if (str === null)
-      fnCancel.call(this);    
+      return fnCancel.call(this);
     fnOk.call(this, str);    
+  },
+
+  showConfirm: function(text, fnOk, fnCancel) {
+    fnCancel = fnCancel || function() {};    
+    if (!window.confirm(text))
+      return fnCancel.call(this);
+    fnOk.call(this);    
   },
 
   showError: function(text, fnOk) {    
@@ -46,16 +68,25 @@ window.MtApp = {
     });
   },
 
-  updateMenuPresets: function() {    
+  updateMenuPresets: function() {     
+    const cur = this.settings.curPreset;   
+    const htmlCheck = '<i class="fa-solid fa-check"></i>';
     const list = $('#topmenuPresets');
     list.html(''); // clear    
     this.settings.presets.forEach(x => {
-      list.append('<li value="'+x.id+'">'+x.name+'</li>');
+      const ch = x.id === cur ? '1' : '0';
+      list.append(`<li value="${x.id}" flag="${ch}">${htmlCheck}${x.name}</li>`);
     });    
   },
 
   updatePreset: function() {
-    // TODO: update preset controls
+    // TODO: update current preset controls and tasks
+  },
+
+  updateSwithFlags: function(con, value) {
+    const list = $(con);
+    list.find('li').attr('flag', 0);
+    list.find(`li[value="${value}"]`).attr('flag', 1);
   },
 
   addPreset: function() {    
@@ -84,6 +115,7 @@ window.MtApp = {
   },
 
   removePreset: function() {
+    const self = this;
     const cur = this.settings.curPreset;
 
     if (cur === 'default')
@@ -96,30 +128,106 @@ window.MtApp = {
 
     const ar = this.settings.presets;
     const index = ar.indexOf(p);
-        
-    ar.splice(index, 1);
 
-    this.updateMenuPresets();
-    this.switchPreset('default');
+    this.showConfirm(`Remove preset "${p.name}"?`, function() {
+      ar.splice(index, 1);
+      self.updateMenuPresets();
+      self.switchPreset('default');      
+    });
   },
 
   switchPreset: function(value) {
-    console.log('switchPreset', value);
+    const cur = this.settings.curPreset;
 
-    // TODO:
+    if (value === cur)
+      return; // already
+    
+    const p = this.getPresetById(value);
 
+    if (!p)
+      return console.error('invalid preset id', value);
+
+    this.settings.curPreset = value;
+
+    this.updateSwithFlags('#topmenuPresets', value);
     this.updatePreset();
     this.settingWrite();
   },
 
+  switchColors: function(value) {
+    const cur = this.settings.curScheme;
+
+    if (value === cur)
+      return; // already
+
+    this.settings.curScheme = value;
+
+    $('body').attr('color-scheme', value);
+
+    this.updateSwithFlags('#topmenuColors', value);
+    this.settingWrite();
+  },
+
   settingWrite: function() {
-    // TODO:
+    const dt = new Date();    
+    this.settings.timestamp = dt.getTime();
+    if (!MtUtils.storageWrite('mt', this.settings)) {
+      console.error('storage writing failed');
+    }
   },
 
   settingsRead: function() {
-    // TODO:
+    const data = MtUtils.storageRead('mt');
+    if (data !== false) {
+      this.settings = data;
+      return true;
+    }
+    return false;
   },
 
+  settingsToFile: function() {
+    MtUtils.saveToFile(this.settings, 'multube.json');
+    this.releaseMenu();
+  },
 
+  settingsFromFile: function() {
+    const self = this;
 
-};
+    MtUtils.loadFromFile(
+      (e) => {
+        if (e === null) return; // cancel
+        
+        const obj = JSON.parse(e.target.result);
+          
+        if (!obj) 
+          return self.showError('json parsing failed');
+
+        if (typeof(obj.version) !== 'string' ||
+            typeof(obj.presets) !== 'object')
+        {
+          return self.showError('invalid or corrupted file');
+        }
+
+        self.settings = obj;
+        self.settingWrite();  
+
+        document.location.reload();
+      }, (e) => {
+        self.showError(e.target.error.name);
+      }, '.json');
+  },
+
+  removeAllTasks: function() {
+    const self = this;
+
+    this.showConfirm(`Remove all tasks?`, function() {
+      // TOOD      
+    });
+  },
+
+  addTaskYt: function() {
+    // TODO
+    this.releaseMenu();
+  },
+
+}; // window.MtApp
--- utils.js
+++ utils.js
@@ -2,14 +2,139 @@
 
 window.MtUtils = {
   
-  // generate unique id
   genUid: function() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   },
 
+  isMobile: function() {
+    return (/Android|iPhone/i.test(navigator.userAgent));
+  },
+
+  hasQueryKey: function(key) {
+    return document.location.href.indexOf('?' + key + '=') != -1 ||
+      document.location.href.indexOf('&' + key + '=') != -1;
+  },
+
+  setCookie: function(c_name, value, exmins) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + (1000 * 60 * exmins));
+    const c_value = escape(value) + ((exmins == null) ? "" : "; expires=" + expires.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+  },
+
+  getCookie: function(c_name) {
+    let i, x, y, cookies = document.cookie.split(";");
+
+    for (i=0; i<cookies.length; i++) {
+        x=cookies[i].substr(0,cookies[i].indexOf("="));
+        y=cookies[i].substr(cookies[i].indexOf("=")+1);
+        x=x.replace(/^\s+|\s+$/g,"");
+        if (x == c_name) {
+            y = unescape(y);
+            if (typeof(y) === 'string' && y == 'null')
+                y = null;
+            if (typeof(y) === 'string' && y == 'true')
+                y = true;
+            if (typeof(y) === 'string' && y == 'false')
+                y = false;
+            return y;
+        }
+    }
+
+    return null;
+  },
+
+  storageWrite: function(key, data) {      
+    if (window.localStorage === undefined) 
+      return false;
+    try {
+      if (!key) 
+        throw new Error('key is required');
+      if (!data) {
+        window.localStorage.removeItem(key);
+      } else {      
+        const str = JSON.stringify(data);
+        window.localStorage.setItem(key, str);
+        console.debug('storageWrite', key + ' length', str.length);
+      }
+      return true;
+    } catch(err) {
+      conosle.error('storageWrite', err);
+      return false;
+    }
+  },
+
+  storageRead: function(key) {      
+    if (window.localStorage === undefined) 
+      return false;
+    try {
+      if (!key) 
+        throw new Error('key is required');
+      const data = window.localStorage.getItem(key);
+      console.debug('storageRead', key + ' length', data ? data.length : 'null');
+      return data ? JSON.parse(data) : false;
+    } catch(err) {
+      conosle.error('storageRead', err);
+      return false;
+    }
+  },
+
+  saveToFile: function(data, fileName, mime) {
+    const str = JSON.stringify(data);
+    const blob = new Blob([str], {type: mime || 'octet/stream'});
+    var url = window.URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    
+    link.href = url;    
+    link.download = fileName || 'file.bin';
+    link.click();
+
+    window.URL.revokeObjectURL(url);
+    link.remove();
+  },
+
+  loadFromFile: function(fnLoaded, fnError, ext) {
+    const self = this;
+    const input = document.createElement('input');    
+
+    document.body.appendChild(input);
+
+    const cleanHtml = function() {
+      input.remove();   
+    };
+
+    $(input).change(function() {
+      let files = input.files; 
+
+      if (files.length == 0) {
+        fnLoaded.call(self, null);
+        cleanHtml();
+        return; 
+      }
+
+      const file = files[0]; 
+      let reader = new FileReader();
+
+      reader.onload = (e) => {   
+        fnLoaded.call(self, e, file);          
+        cleanHtml();
+      };
+ 
+      reader.onerror = (e) => {
+        fnError.call(self, e);        
+        cleanHtml();
+      }
+
+      reader.readAsText(file);
+    });
+
+    input.type = 'file'; 
+    input.accept = ext || '.json';
+    input.click();
+  },
+
 };
-
-
-
